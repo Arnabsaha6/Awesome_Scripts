8790b78dce637ca6b3b3a167ecbccbc2bdd2f447
Debangshi Mandal <70003464+debangshimandal@users.noreply.github.com>
2020-10-31 16:22:20 +0530

    Update scripts.sh

--- scripts.sh
+++ scripts.sh
@@ -42,3 +42,16 @@ USE=`df -h |grep $PART | awk '{ print $5 }' | cut -d'%' -f1`
 if [ $USE -gt $MAX ]; then
   echo "Percent used: $USE" | mail -s "Running out of disk space" $EMAIL
 fi
+#Brightness
+echo "enter value rangimg from 5 to 10"
+read x
+y=4
+#safety check
+if [ $x -gt $y ] && [ $x -lt 20 ]
+then
+  z=$(echo "scale=2;$x / 10" | bc)
+  xrandr --output eDP1 --brightness $z
+ echo "brightness changed: $z"
+else
+  echo "Wrong input , enter value greater than 4 and less than 20"
+fi
